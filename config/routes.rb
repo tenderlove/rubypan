160a79ecca1fda90619b4d662f20b5ac7372b713
Eric Hodel <drbrain@segment7.net>
2009-04-16 16:23:14 -0700

    Fix routes for searching

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "results"
+  map.root :controller => 'results'
   map.connect ':action/:id', :controller => 'results'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect ':action.:format', :controller => 'results'
 end
