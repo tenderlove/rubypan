c7e51139beaf67273a56a0b528d4021c3950e148
Eric Hodel <drbrain@segment7.net>
2009-04-16 15:07:34 -0700

    Bare-bones gem plugin support

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -7,5 +7,19 @@ class ResultsController < ApplicationController
     @releases = Release.search(params[:q].sub('.', '')).latest.find(:all,
       :include => [:ruby_gem => :authors]
     )
+
+    if params[:f] == 'Marshal' then
+      response.charset = nil
+      response.content_type = 'application/octet-stream'
+      response.header['Content-Transfer-Encoding'] = 'binary'
+
+      source = 'http://gems.rubyforge.org/'
+      releases = @releases.map do |release|
+        [[release.ruby_gem.name, Gem::Version.new(release.version),
+          Gem::Platform::RUBY], source]
+      end
+
+      render :text => Marshal.dump(releases)
+    end
   end
 end
--- test/functional/results_controller_test.rb
+++ test/functional/results_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class ResultsControllerTest < ActionController::TestCase
+  
+  fixtures :releases, :ruby_gems, :authors
+
   test "search provides releases" do
-    get :search
+    get :search, :q => 'foobar'
     assert assigns(:releases)
+  end
+
+  test "search returns Marshal" do
+    get :search, :f => 'Marshal', :q => 'nokogiri'
+    assert assigns(:releases)
+    assert_equal 'application/octet-stream', @response.headers['content-type']
+
+    expected = [
+      [['nokogiri', Gem::Version.new('1.0.1'), Gem::Platform::RUBY],
+       'http://gems.rubyforge.org/']
+    ]
+
+    assert_equal expected, Marshal.load(@response.body)
   end
 
   test "index redirects to search when there is a q" do
