c0c419ab3ac39af431a12e60767c3e74d97e282e
Mark Holton <holtonma@Macintosh-69.local>
2009-04-20 00:59:25 -0700

    started writing tests, but i can't get them to run, think i have environment issues setting up Postgres

--- test/functional/results_controller_test.rb
+++ test/functional/results_controller_test.rb
@@ -8,20 +8,20 @@ class ResultsControllerTest < ActionController::TestCase
     get :search, :q => 'foobar'
     assert assigns(:releases)
   end
-
+  
   test "search returns Marshal" do
     get :search, :format => 'Marshal', :q => 'nokogiri'
     assert assigns(:releases)
     assert_equal 'application/octet-stream', @response.headers['content-type']
-
+  
     expected = [
       [['nokogiri', Gem::Version.new('1.0.1'), Gem::Platform::RUBY],
        'http://gems.rubyforge.org/']
     ]
-
+  
     assert_equal expected, Marshal.load(@response.body)
   end
-
+  
   test "index redirects to search when there is a q" do
     get :index, :q => 'foobar'
     assert_redirected_to :action => 'search'
--- test/unit/rating_test.rb
+++ test/unit/rating_test.rb
@@ -2,8 +2,5 @@ require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
   fixtures :releases, :authors, :ruby_gems
-  # Replace this with your real tests.
-  test "i dunno" do
-    assert true
-  end
+  
 end
